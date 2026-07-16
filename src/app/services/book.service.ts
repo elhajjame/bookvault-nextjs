89937411c59942ea95cf63f345b27222b527739c
Mehdi El-Hajjame <el-hajjame@outlook.com>
2026-07-16 10:35:44 +0100

    feat: add Zod validation for book input and implement book service functions

---
+++ src/app/services/book.service.ts
@@ -0,0 +1,37 @@
+import { connectDb } from "../lib/db";
+import { BookInput } from "../lib/validators";
+import Book from "../models/Book";
+
+export async function getAllBooks() {
+  await connectDb();
+
+  return Book.find().sort({ createdAt: 1 });
+}
+
+export async function getBookById(id: string) {
+  await connectDb();
+
+  return Book.findById(id);
+}
+
+export async function createBook(data: BookInput) {
+  const existing = await Book.findOne({ isbn: data.isbn });
+
+  if (existing) {
+    throw new Error("A book with this ISBN already exists.");
+  }
+
+  return Book.create(data);
+}
+
+export async function updateBook(id: string, data) {
+  await connectDb();
+
+  return Book.findByIdAndUpdate(id, data, { new: true, runValidator: true });
+}
+
+export async function deleteBook(id: string) {
+  await connectDB();
+
+  return Book.findByIdAndDelete(id);
+}
